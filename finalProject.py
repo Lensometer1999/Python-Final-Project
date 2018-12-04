763d3ac08eba4975be16ef2a60699de69734c118
Jacob Lucas <44979131+Lensometer1999@users.noreply.github.com>
2018-12-03 21:03:32 -0600

    Final Revision
    Final version of code for presentation.

--- finalProject.py
+++ finalProject.py
@@ -1,7 +1,7 @@
 from Crypto.Cipher import AES
 import base64
 
-# the block size for the cipher object
+# the block size for the cipher object, it must be 16 for 128 bit encryption to work
 BLOCK_SIZE = 16
 
 # the character used for padding--with a block cipher such as AES, the value
@@ -9,6 +9,7 @@ BLOCK_SIZE = 16
 # used to ensure that your value is always a multiple of BLOCK_SIZE
 PADDING = '{'
 
+# the lambda operator or lambda function is used for creating small, one-time and anonymous function objects in Python.
 # pads the text to be encrypted
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
 
@@ -16,6 +17,7 @@ pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
 EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
 
 # creating a key by your own choice and checking to make sure it is 16 characters long
+# using an infinite while loop to error check
 while True: 
     secret = input("Please Enter An Encryption Key (it must be 16 characters long): ")
     countTotal= (len(secret))
